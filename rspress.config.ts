98c25300da5f56f181ac497b7a00de9f37a92eb2
David Serrano <75829857+serranoio@users.noreply.github.com>
2025-12-15 08:51:17 -0800

    done

--- rspress.config.ts
+++ rspress.config.ts
@@ -4,11 +4,43 @@ import { defineConfig } from 'rspress/config';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'Bachatorial',
+  description: 'Embrace',
   icon: '/rspress-icon.png',
   logoText: 'Bachatorial',
   // logo: {
   //   light: '/rspress-light-logo.png',
   //   dark: '/rspress-dark-logo.png',
   // },
+  head: [
+    // OpenGraph meta tags for social media sharing
+    ['meta', { property: 'og:title', content: 'Bachatorial' }],
+    [
+      'meta',
+      {
+        property: 'og:description',
+        content: 'Embrace life thru a bachata dance lesson',
+      },
+    ],
+    [
+      'meta',
+      { property: 'og:image', content: 'https://bachatorial.com/logo.png' },
+    ],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:url', content: 'https://bachatorial.com' }],
+    // Twitter Card meta tags
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:title', content: 'Bachatorial' }],
+    [
+      'meta',
+      {
+        name: 'twitter:description',
+        content: 'Embrace life thru a bachata dance lesson',
+      },
+    ],
+    [
+      'meta',
+      { name: 'twitter:image', content: 'https://bachatorial.com/logo.png' },
+    ],
+  ],
   themeConfig: {},
 });
